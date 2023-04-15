ab15bf82d9e53fe28650c2942e450420c8cede86
garry <garry4978@gmail.com>
2023-04-16 02:45:58 +0300

    init

---
+++ src/main/java/com/skypro/adsonline/AdsOnlineApp.java
@@ -0,0 +1,13 @@
+package com.skypro.adsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsOnlineApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsOnlineApp.class, args);
+    }
+
+}
